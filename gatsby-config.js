d37f90538fb30db18f43be34f83cf42c366e1e20
Juan Sanchez <juan.sanchez1192@gmail.com>
2019-04-12 18:17:31 +0200

    Add amplify stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Amplify example`,
+    title: `Gatsby Amplify development`,
     description: `Hello there!`,
     author: `@gatsbyjs`,
   },
